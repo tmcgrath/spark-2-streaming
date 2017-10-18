ab852b825b11867308738e70110583e92b8665ae
Todd McGrath <mcgrath@supergloo.com>
2017-10-18 18:25:44 -0500

    backing up

--- src/main/scala/com/supergloo/SparkKinesisExample.scala
+++ src/main/scala/com/supergloo/SparkKinesisExample.scala
@@ -1,8 +1,5 @@
 package com.supergloo
 
-import java.nio.ByteBuffer
-
-import scala.util.Random
 import com.amazonaws.auth.{BasicAWSCredentials, DefaultAWSCredentialsProviderChain}
 import com.amazonaws.regions.RegionUtils
 import com.amazonaws.services.kinesis.AmazonKinesisClient
@@ -15,50 +12,47 @@ import org.apache.spark.streaming.dstream.DStream.toPairDStreamFunctions
 import org.apache.spark.streaming.kinesis.KinesisUtils
 
 /**
-  * For now, just a copy of Word count example
+  * // TODO
+  * See http://www.supergloo.com/fieldnotes
   */
 object SparkKinesisExample extends Logging {
 
   def main(args: Array[String]) {
 
-    val conf = new SparkConf().setAppName("KinesisWordCountASL")
+    val conf = new SparkConf().setAppName("Kinesis Read Sensor Data")
     conf.setIfMissing("spark.master", "local[*]")
 
-    // Check that all required args were passed in.
-    if (args.length != 3) {
-      System.err.println(
-        """
-          |Usage: KinesisWordCountASL <app-name> <stream-name> <endpoint-url> <region-name>
-          |
-          |    <app-name> is the name of the consumer app, used to track the read data in DynamoDB
-          |    <stream-name> is the name of the Kinesis stream
-          |    <endpoint-url> is the endpoint of the Kinesis service
-          |                   (e.g. https://kinesis.us-east-1.amazonaws.com)
-          |
-          |Generate input data for Kinesis stream using the example KinesisWordProducerASL.
-          |See http://spark.apache.org/docs/latest/streaming-kinesis-integration.html for more
-          |details.
-        """.stripMargin)
-      System.exit(1)
-    }
+//    // Check that all required args were passed in.
+//    if (args.length != 3) {
+//      System.err.println(
+//        """
+//          |Usage: KinesisWordCountASL <app-name> <stream-name> <endpoint-url> <region-name>
+//          |
+//          |    <app-name> is the name of the consumer app, used to track the read data in DynamoDB
+//          |    <stream-name> is the name of the Kinesis stream
+//          |    <endpoint-url> is the endpoint of the Kinesis service
+//          |                   (e.g. https://kinesis.us-east-1.amazonaws.com)
+//          |
+//          |Generate input data for Kinesis stream using the example KinesisWordProducerASL.
+//          |See http://spark.apache.org/docs/latest/streaming-kinesis-integration.html for more
+//          |details.
+//        """.stripMargin)
+//      System.exit(1)
+//    }
 
  //   StreamingExamples.setStreamingLogLevels()
-
+    //TODO - typesafe config rather than command
     val Array(appName, streamName, endpointUrl) = args
-
-    println(s"APP: ${appName} STREAM: ${streamName} ENDPOINT ${endpointUrl}")
 
     val credentials = new DefaultAWSCredentialsProviderChain().getCredentials()
     require(credentials != null,
-      "No AWS credentials found. Please specify credentials using one of the methods specified " +
-        "in http://docs.aws.amazon.com/AWSSdkDocsJava/latest/DeveloperGuide/credentials.html")
+      "No AWS credentials found. See http://docs.aws.amazon.com/AWSSdkDocsJava/latest/DeveloperGuide/credentials.html")
     val kinesisClient = new AmazonKinesisClient(credentials)
     kinesisClient.setEndpoint(endpointUrl)
     val numShards = kinesisClient.describeStream(streamName).getStreamDescription().getShards().size
 
     val numStreams = numShards
 
-    // Spark Streaming batch interval
     val batchInterval = Milliseconds(2000)
 
     val kinesisCheckpointInterval = batchInterval
@@ -67,8 +61,6 @@ object SparkKinesisExample extends Logging {
     // DynamoDB of the same region as the Kinesis stream
     val regionName = RegionUtils.getRegionByEndpoint(endpointUrl).getName()
 
-    // Setup the SparkConfig and StreamingContext
-//    val sparkConfig = new SparkConf().setAppName("KinesisWordCountASL")
     val ssc = new StreamingContext(conf, batchInterval)
 
     // Create the Kinesis DStreams
@@ -81,10 +73,21 @@ object SparkKinesisExample extends Logging {
     val unionStreams = ssc.union(kinesisStreams)
 
     // Convert each line of Array[Byte] to String, and split into words
-    val words = unionStreams.flatMap(byteArray => new String(byteArray).split(" "))
 
+    val words = unionStreams.map { byteArray =>
+      val Array(sensorId, temp, status) = new String(byteArray).split(",")
+      SensorData(sensorId, temp, status)
+    }
+
+    // TODO - save raw data to cassandra
+
+    // TODO - save aggregated to someplace else
+
+    println(s"WORDS: ${words} size: ${words.count()}")
     // Map each word to a (word, 1) tuple so we can reduce by key to count the words
     val wordCounts = words.map(word => (word, 1)).reduceByKey(_ + _)
+
+    // TODO - add windowing?  sensor's highest value over the past 10 min?
 
     // Print the first 10 wordCounts
     wordCounts.print()
@@ -94,3 +97,4 @@ object SparkKinesisExample extends Logging {
     ssc.awaitTermination()
   }
 }
+case class SensorData(id: String, currentTemp: String, status: String)
